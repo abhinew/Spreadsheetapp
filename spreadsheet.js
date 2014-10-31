3ee1b2884e8bb9f6264df11b2e7f9586678c69fa
Abhinaya <abhinaya@gmail.com>
2014-10-31 16:06:16 +0530

    ColoumnHeads implemented

--- spreadsheet.js
+++ spreadsheet.js
@@ -4,24 +4,32 @@
 
 $(document).ready(function(){
     $("#create").click(function(){
-
-
-
             var spreadsheetStructure = {
                 name: "Abhi's spreadsheet",
                 coloumnHeads : []
             };
-            var a = 97;
-            var charArray = [];
+        var a = 65;
+
+        var charArray = [];
+
+        for (var i = 0; i<26; i++)
+        {
+            charArray.push(String.fromCharCode(a + i));
+        }
+
+        while (charArray.length != 0)
+        {
+            var firstletter = charArray.shift();
             for (var i = 0; i<26; i++)
             {
-                charArray.push(String.fromCharCode(a + i));
+                spreadsheetStructure.coloumnHeads.push(firstletter+String.fromCharCode(a+i));
             }
+        }
 
-            for (var i=0; i<100; i++ )
+            /*for (var i=0; i<100; i++ )
             {
                 spreadsheetStructure.coloumnHeads.push({ name: 'A'});
-            }
+            }*/
             var template = document.getElementById('templates').innerHTML;
             var output = Mustache.render(template, spreadsheetStructure);
             $("#excel").html(output);
